f3fbbe051ee5e5b2a4d4bbb0f4b73cb28fb01708
Kenya <kenya@Andrews-MacBook-Pro-2.local>
2019-01-11 13:14:57 -0600

    update on files

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 app.use( express.static( "public" ) );
 
+
+// prepare server
+
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 // index page
 app.get('/', function(req, res) {
     res.render('page/index');
